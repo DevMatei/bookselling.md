ad700932ac107ec6b378be0776093972d6ce9e2a
DevMatei <matei.thoma@gmail.com>
2023-01-02 14:52:02 +0200

    deleted code

--- assets/JS codes/Index-JS.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for your message! We will get back to you as soon as possible.");
-    });
