ccb367c240c934b04ede6ca7c316a73f336f9fba
DevMatei <matei.thoma@gmail.com>
2023-01-02 14:34:48 +0200

    modified code

---
+++ assets/JS codes/Index-JS.js
@@ -0,0 +1,4 @@
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for your message! We will get back to you as soon as possible.");
+    });
