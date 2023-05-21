9724ebbf4121b3a6b931ca58402caa5ce2858c9b
DevMatei <matei.thoma@gmail.com>
2023-05-21 15:01:36 +0300

    Super Slider changes

--- assets/JS codes/Index-JS.js
+++ assets/JS codes/Index-JS.js
@@ -1,9 +1,10 @@
 const left = document.getElementById("left-side");
 
-const handleMove = e => {
-  left.style.width = `${e.clientX / window.innerWidth * 100}%`;
-}
+const handleMove = (e) => {
+  const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+  const widthPercentage = (clientX / window.innerWidth) * 100;
+  left.style.width = `${widthPercentage}%`;
+};
 
-document.onmousemove = e => handleMove(e);
-
-document.ontouchmove = e => handleMove(e.touches[0]);
+document.onmousemove = (e) => handleMove(e);
+document.ontouchmove = (e) => handleMove(e);
